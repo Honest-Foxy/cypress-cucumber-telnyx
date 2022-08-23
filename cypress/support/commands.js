7c1b4d77fee487437a0e0720bc875e4c111331fe
Honest-Foxy <malakhova.dariya@gmail.com>
2022-08-23 20:28:35 +0000

    Update from https://github.com/Honest-Foxy/cypress-cucumber-telnyx/commit/1ce3a5623b49e8bb5b118e92f26e24a8f40fc879

--- cypress/support/commands.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add('openMainURL', () => {
-    const mainPage = require('./pageObjects/main.page');
-    cy.clearCookies();
-    mainPage.openMainURL();
-    mainPage.denyCookies();
-})
